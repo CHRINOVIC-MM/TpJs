92211cb931e5f1a975473f701580e3f4d733a932
Chrinovic MM <20mm364@esisalama.org>
2022-10-18 20:43:01 +0200

    Fin de toutes les questions me reste 6

---
+++ app.js
@@ -0,0 +1,118 @@
+//question 1
+
+/*let chiffre = parseInt(prompt("Entrez un nombre entre 1 et 10"));
+
+switch (chiffre) {
+    case 1:
+        alert("Vous avez choisi le chiffre" + chiffre);
+        break;
+    
+    case 2:
+        alert("Vous avez choisi le chiffre" + chiffre);
+        break;
+
+    case 3:
+        alert("Vous avez choisi le chiffre" + chiffre);
+        break;
+
+    case 4:
+        alert("Vous avez choisi le chiffre" + chiffre);
+        break;
+
+    case 5:
+        alert("Vous avez choisi le chiffre" + chiffre);
+        break;
+
+    case 6:
+        alert("Vous avez choisi le chiffre" + chiffre);
+        break;
+
+    case 7:
+        alert("Vous avez choisi le chiffre" + chiffre);
+        break;
+
+    case 8:
+        alert("Vous avez choisi le chiffre" + chiffre);
+        break;
+
+    case 9:
+        alert("Vous avez choisi le chiffre" + chiffre);
+        break;
+
+    case 10:
+        alert("Vous avez choisi le chiffre" + chiffre);
+        break;
+
+    default:
+        alert("Vous avez échouer ")
+        break;
+}*/
+
+//question 2
+
+/*let age = parseInt(prompt("Entrez votre age : "));
+
+if (age < 0) {
+    alert("vous n’avez pas d’âge");
+}
+else if(age >= 0 && age<=5){
+    alert("vous êtes bébé");
+}
+else if (age >= 6 && age<= 12) {
+    alert("Vous êtes gamin");
+}
+else if (age >= 13 && age <= 18) {
+    alert("Vous êtes ado");
+}
+
+else if (age >= 19 && age <= 31) {
+    alert("Vous êtes adulte ");
+}
+else{
+    alert("Bonjour, vous êtes bien papy");
+}*/
+
+//question 3
+
+/*let nombre1 = parseInt(prompt("entrez le 1er nombre"));
+let nombre2 = parseInt(prompt("entrez le 2er nombre"));
+let nombre3 = parseInt(prompt("entrez le 3er nombre"));
+
+if(nombre1 && nombre2 && nombre3){
+    if(typeof(nombre1)!= Number || typeof(nombre2) != Number || typeof(nombre3) != Number){
+        alert("Toutes les valeurs doivent etre numerique");
+    }else{
+        if(nombre1 === nombre2 && nombre1 === nombre3){
+            alert("Les trois variables sont identiques !")
+        }else if (nombre1 === nombre2 || nombre1 === nombre3) {
+            alert("deux des variables sont de valeurs égales")
+        }else{
+            alert("es trois variables sont différentes")
+        }
+    }
+}*/
+
+//question 4
+
+/*for(let i =0; i < 11; i++){
+    for(let j=1; j< i; j++){
+        console.log("1\n");
+    }
+}*/
+
+
+//question 5
+
+/*let resultat;
+for(let i = 1; i <= 50; i++){
+    resultat = i*13;
+
+    if(resultat % 7 === 0){
+        document.write(resultat + "  ");
+    }
+}*/
+
+
+
+//question 6
+
